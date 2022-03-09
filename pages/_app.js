de991b103ddcb259f18e2a8eeb0f72eb3552a0ef
andrewpltnv <andrewpltnv@gmail.com>
2022-03-09 17:20:28 +0200

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,50 @@
 import Layout from '../components/layout'
 import '../styles/globals.scss'
 import 'modern-css-reset/dist/reset.min.css'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-export default function MyApp({Component, pageProps}) {
+const MyApp = ({ Component, pageProps }) => {
+  console.log(gtag.GA_TRACKING_ID);
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <Layout Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Layout Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
+
+export default MyApp
