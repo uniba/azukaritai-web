825c924ee5879bff33b18f7e30d7da60ef7883ee
cazmura <49217331+cazmura@users.noreply.github.com>
2022-05-18 11:24:51 +0900

    WORKS の画像差し替え

--- pages/works.js
+++ pages/works.js
@@ -16,7 +16,7 @@ export default function Works() {
         </div>
         <div className="p-about__work">
           <div className="p-about__mb-container--odd">
-            <div className="p-about__mb-image"><img src="img/az_jun.png" /></div>
+            <div className="p-about__mb-image"><img src="img/shinkenchiku-works.png" /></div>
             <p className="p-about__mb-name">新建築 WEBサービス</p>
             <p className="p-about__mb-intro">株式会社新建築社が運用する<a href="https://id.shinkenchiku.online/">『新建築ID』</a><a href="https://compe.shinkenchiku.online/">『新建築コンペ』</a><a href="https://shinkenchiku.online/">『新建築オンライン』</a>。株式会社新建築データが運用する<a href="https://data.shinkenchiku.online/">『新建築データ』</a>。これらのサービスの運用と改修業務に取り組んでいます。新規開発を得意とする<a href="https://z-1.works/">Z1</a>チームと連携し、開発と運用の両面からクライアントのビジネスをサポートしています。</p>
           </div>
