5bb5a97ed2fae334be563d69b8414800596f4938
cazmura <49217331+cazmura@users.noreply.github.com>
2022-05-31 23:33:14 +0900

    contactLink() のリンクをGoogleフォームに変更

--- components/ContactLink.js
+++ components/ContactLink.js
@@ -4,7 +4,7 @@ export default function contactLink() {
       <h3 className="p-contact-info__title">CONTACT</h3>
       <p className="p-contact-info__text">お気軽にお問い合わせ下さい。</p>
       <div className="p-contact-info__load-link">
-        <a href="https://form.asana.com?k=ggLvwV5Jy4v6e9WhagMCjg&d=10976309821910" target="_blank" rel="noopener noreferrer">Read more</a>
+        <a href="https://forms.gle/oLb5j4kcFACsBWGy8" target="_blank" rel="noopener noreferrer">Read more</a>
       </div>
     </div>
   )
